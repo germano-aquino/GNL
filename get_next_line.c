ac9b536b7e562e38d1b29adf16acc42077c871d3
Germano Aquino <germano.r.aquino@gmail.com>
2021-11-09 23:56:24 -0300

    Code with norm

--- get_next_line.c
+++ get_next_line.c
@@ -1,65 +1,87 @@
-#ifndef BUFFER_SIZE
- #define BUFFER_SIZE 0
-#endif
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 0
+#endif
+
+int	ft_read_from_fd(char **buff_add, int fd)
+{
+	int		sz;
+	char	*buff;
+
+	buff = *buff_add;
+	sz = read(fd, buff, BUFFER_SIZE);
+	if (!sz || sz == -1)
+		return (0);
+	buff[sz] = '\0';
+	return (1);
+}
+
+char	*ft_realloc(char *ptr, int *size)
+{
+	char	*new;
+	int		index;
+
+	*size += BUFFER_SIZE;
+	new = (char *) malloc(sizeof(char) * *size);
+	if (!new)
+		return (NULL);
+	index = 0;
+	while (ptr[index] != '\0')
+	{
+		new[index] = ptr[index];
+		index++;
+	}
+	new[index] = '\0';
+	free(ptr);
+	return (new);
+}
+
+char	*ft_get_line(char **buff_add, int fd)
+{
+	char	*line;
+	int		line_size;
+	int		index;
+	int		line_not_end;
+
+	line_size = BUFFER_SIZE + 1;
+	line = (char *) malloc(sizeof(char) * line_size);
+	if (!line)
+		return (NULL);
+	index = 0;
+	line_not_end = 1;
+	while (line_not_end)
+	{
+		if (**buff_add == '\0' && !ft_read_from_fd(buff_add, fd))
+			return (line);
+		if (index && index % BUFFER_SIZE == 0)
+			line = ft_realloc(line, &line_size);
+		line[index] = **buff_add;
+		if (**buff_add == '\n')
+			line_not_end = 0;
+		(*buff_add)++;
+		index++;
+	}
+	line[index] = '\0';
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	*buff;
-	char	*line;
-	int		index;
-	int		line_size;
-	int		sz;
+	char		*line;
 
 	if (BUFFER_SIZE <= 0 || fd < 0 || fd > 255)
 		return (NULL);
 	if (buff == NULL)
 	{
 		buff = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!buff)
+		if (!buff || !ft_read_from_fd(&buff, fd))
 			return (NULL);
-		sz = read(fd, buff, BUFFER_SIZE);
-		if (!sz || sz == -1)
-			return (NULL);
-		buff[sz] = '\0';
 	}
-	line_size = BUFFER_SIZE;
-	line = (char *) malloc(sizeof(char) * (line_size + 1));
-	index = 0;
-	if (*buff == '\n')
-		buff++;
-	if (*buff == '\0')
-	{
-		sz = read(fd, buff, BUFFER_SIZE);
-		buff[sz] = '\0';
-	}
-	line[index] = *buff;
-	while (*buff != '\n')
-	{
-		buff++;
-		index++;
-		if (index % BUFFER_SIZE == 0)
-		{
-			line_size += BUFFER_SIZE;
-			line = (char *) realloc(line, line_size + 1);
-		}
-		if (*buff == '\0')
-		{
-			sz = read(fd, buff, BUFFER_SIZE);
-			if (!sz || sz == -1)
-			{
-				if (!line[0])
-					return (NULL);
-				break;
-			}
-			buff[sz] = '\0';
-		}
-		line[index] = *(buff);
-	}
-	index++;
-	line[index] = '\0';
+	line = ft_get_line(&buff, fd);
+	if (!line[0])
+		return (NULL);
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,1 +1,11 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+int		ft_read_from_fd(char **buff_add, int fd);
+
+char	*ft_realloc(char *ptr, int *size);
+
+char	*ft_get_line(char **buff_add, int fd);
+
 char	*get_next_line(int fd);
+
+#endif
